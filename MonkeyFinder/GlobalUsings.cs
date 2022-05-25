0ff439624cab3c91606117d7a173e188e25623bf
Michael Bond <codemonkey8510@icloud.com>
2022-05-25 19:49:16 -0400

    Get monkeys from web

--- MonkeyFinder/GlobalUsings.cs
+++ MonkeyFinder/GlobalUsings.cs
@@ -8,3 +8,4 @@ global using System.Diagnostics;
 global using System.Runtime.CompilerServices;
 global using System.Text.Json;
 global using MonkeyFinder.View;
+global using System.Net.Http.Json;
--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,23 @@
 ﻿namespace MonkeyFinder.Services;
 
-public class MonkeyService
+public record MonkeyService(HttpClient HttpClient)
 {
+    private List<Monkey> monkeyList = new();
+
+    public async Task<List<Monkey>> GetMonkeysAsync()
+    {
+        if (monkeyList.Count > 0)
+        {
+            return monkeyList;
+        }
+
+        var response = await HttpClient.GetAsync("https://montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        return monkeyList;
+    }
 }
