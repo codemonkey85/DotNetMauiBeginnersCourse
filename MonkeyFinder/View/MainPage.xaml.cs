b95f8f08e24c9cf5de84585f22056a9056e10d9f
Michael Bond <codemonkey8510@icloud.com>
2022-05-25 19:22:45 -0400

    A bunch of work

---
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MonkeyFinder.View;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    private bool isBusy;
+    private string title;
+
+    public bool IsBusy
+    {
+        get => isBusy;
+        set
+        {
+            if (isBusy == value)
+            {
+                return;
+            }
+
+            isBusy = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(IsNotBusy));
+        }
+    }
+
+    public string Title
+    {
+        get => title;
+        set
+        {
+            if (title == value)
+            {
+                return;
+            }
+
+            title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public bool IsNotBusy => !IsBusy;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
