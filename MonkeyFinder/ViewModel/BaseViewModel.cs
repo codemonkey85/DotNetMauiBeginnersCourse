f1ae571abe147b6aa4651b748c4290e33e1b5da5
Michael Bond <codemonkey8510@icloud.com>
2022-05-25 19:31:19 -0400

    Better MVVM

--- MonkeyFinder/ViewModel/BaseViewModel.cs
+++ MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,44 +1,9 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
-    private bool isBusy;
-    private string title;
-
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-            {
-                return;
-            }
-
-            isBusy = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
-    }
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value)
-            {
-                return;
-            }
-
-            title = value;
-            OnPropertyChanged();
-        }
-    }
+    [ObservableProperty] private bool isBusy;
+    [ObservableProperty] private string title;
 
     public bool IsNotBusy => !IsBusy;
-
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    public void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
